e63b41ce03d991978335402b619dd32818c181e8
Hanson <h@hanc.cc>
2022-09-01 16:55:19 +0800

    ok

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # demo_keyword_reply
+
+基于 xbot 实现微信关键词自动回复
+
+## 使用
+
+先配置好 xbot 的配置如下，并运行
+```
+CALLBACK_URL=http://127.0.0.1:5678
+SECRET=xxx
+PORT=5557
+```
+
+clone 代码
+```
+git clone https://github.com/xbot-org/demo_keyword_reply.git
+cd demo_keyword_reply
+```
+
+运行此服务 `php -S localhost:5678`
+
+## 效果图
+
+![1662022320_1_.png](https://s2.loli.net/2022/09/01/3ejwHTLXsOQVlI2.png)
+
+## xbot 文档
+
+https://www.apifox.cn/apidoc/shared-71b9855b-693c-48fc-858b-cde2c5afe5a8
---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+
+$jsonString = file_get_contents("php://input");
+
+$msg = json_decode($jsonString, true);
+
+switch ($msg['type']) {
+    case 'MT_RECV_TEXT_MSG':
+        if ($msg['data']['msg'] == 'ping') {
+            sendText($msg['data']['from_wxid'], 'pong');
+        } elseif ($msg['data']['msg'] == '123') {
+            sendText($msg['data']['from_wxid'], '456');
+        }
+}
+
+echo '{}';
+
+function sendText($wxid, $content) {
+    post(json_encode([
+        'client_id' => 1,
+        'is_sync' => 1,
+        'data' => [
+            'to_wxid' => $wxid,
+            'content' => $content,
+        ],
+        'type' => 'MT_SEND_TEXTMSG',
+    ]));
+}
+
+function post($data) {
+    $opts = array('http' =>
+        array(
+            'method'  => 'POST',
+            'header'  => 'Content-Type: application/json',
+            'content' => $data
+        )
+    );
+
+    $context  = stream_context_create($opts);
+
+    file_get_contents('http://127.0.0.1:5557', false, $context);
+}
